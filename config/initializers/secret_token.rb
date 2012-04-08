6aed7f0e97e4e3cb250cdfa27a94d031b5a6eebb
David Parry <david.parry@suranyami.com>
2012-04-08 17:08:24 +1000

    Created a rails app for deploying on heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vintography::Application.config.secret_token = '5b98dc09a05ba05f4a3dbf0a65dd6e81f97d12edcf011925f3408b1d20908f0beedfebc3d122efcd99b0b8ecfd2922fa08c04d2bf48589f7539869b272298218'
